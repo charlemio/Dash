f956fbb0b903bcb4adffa9a63d6e264b3b4f8de9
Christoffer Alenskog <chrab@kth.se>
2021-08-08 15:54:13 +0200

    Try to fix netlify error

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
+import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +10,7 @@ const config = {
     })],
 
 	kit: {
-		adapter: adapter(),
+		adapter: netlify(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 	}
